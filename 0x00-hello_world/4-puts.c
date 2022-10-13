a6e04b71cf30217c770758db6897455a05fab21e
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-13 10:29:14 -0700

    fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
- */
-int main(void)
-{
-	puts("\"Programming is like building a multi
-lingual puzzle");
-	return (0);
-}
+ * Return: Alway}
