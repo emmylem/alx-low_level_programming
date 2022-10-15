a726120467e1e7581e904a1b2ce1ca9a45473a66
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-14 17:01:46 -0700

    task 2 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * nain - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = "a"; lc <= "z"; lc++);
+	{
+		putchar(lc);
+	}
+	putchar("\n");
+	return (0);
+}
+
