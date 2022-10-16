72df7c3fb4fdefacc6519a4e1ac493664af15930
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-16 10:21:53 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 18; num < 28 num++)
+	{
+		putchar(num);
+		if (num != 27)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+
+		putchar('\n');
+
+		return (0);
+	}
