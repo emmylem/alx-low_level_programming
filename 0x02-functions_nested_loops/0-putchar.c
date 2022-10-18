56162aa91004a565edb7b2b159c685ad3c3ab274
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-18 11:14:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "flox.h"
+/**
+ * main - prints putchar and a new line
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar(',');
+	_putchar('y');
+	_putchar('g');
+	_putchar('m');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "flox.h"
+#include <unistd.h>
+/**
+ * flox - Entry header
+ * @c - the character to print
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
