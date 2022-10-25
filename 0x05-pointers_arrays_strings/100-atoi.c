a1d53a00a7d93e83c7ed35669a6bf9cb4f6c34de
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-25 15:46:24 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * _atoi - converts a string to an integer
- * @s: string
+ * @s: the string
  * Return: value
  */
 int _atoi(char *s)
 {
-
 	int sign = 1;
 	unsigned int num = 0;
 
-	while (!('0' <= *s && *s <= '9') && *s != '0')
+	while (!('0' <= *s && *s <= '9') && *s != '\0')
 	{
 		if (*s == '-')
 			sign *= -1;
@@ -21,10 +19,8 @@ int _atoi(char *s)
 	}
 	while ('0' <= *s && *s <= '9' && *s != '\0')
 	{
-		num = (num * 10) + (*s - '0');
+		num = (num * 10) + (*s - 10);
 		s++;
 	}
-	printf("Hello ----- world\n");
-
 	return (num * sign);
 }
