041d2798e68310c3ab8f6356fba7552f3331f589
emmylemk9 <benjaminosondu55@gmail.com>
2022-10-25 14:17:00 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string
+ * Return: value
+ */
+int _atoi(char *s)
+{
+	char s[] = "210";
+
+	int x = atoi(s);
+
+	printf("The number is %d\n", x);
+	return (0);
+}
