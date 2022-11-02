5df171e5b0ecb6ce233e68287842271aa28610d1
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-02 14:48:05 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns square root
+ * find_sqrt - finds the natural root
+ * @root: dk
+ * @num:
+ * _sqrt_recursion - returns square root of a number
+ * Return: sqrt
+ */
+int find_sqrt(int num, int root);
+int _sqrt_recursion(int n)
+{
+	int num;
+	int root;
+
+	if ((root * root) == num)
+		return (root);
+
+	return (0);
+}
