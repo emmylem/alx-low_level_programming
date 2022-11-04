87e65de9ec5c68db2eab3f00c851350e8077689f
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-04 09:06:40 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - funtion to print its name
+ * @argc: parameter to count
+ * @argv: array of command
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%s\n", argv);
+	return (0);
+}
