a6aaf0d8ba2f495cccace00f1b40f63c4faa0613
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-04 11:28:54 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 {
 	int result = 0, x, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
-
 	for (i = 0; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
 			printf("%s\n", "Error");
 			return (1);
+			}
 		}
 	}
 
