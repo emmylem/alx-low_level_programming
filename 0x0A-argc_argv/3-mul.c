0a82c9d72be2a8bede190521f18c3bcd35cf604c
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-04 10:48:34 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - multiply
+ * @argc: parameter
+ * @argv: array of command
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result, x, y;
+
+	if (argc > 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+
+	printf("%d\n", result);
+	return (0);
+}
