e683d4a732d77a228075ee7432fab63f263dfc5c
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-04 09:53:04 -0700

    t

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number
+ * @argc: the parameter
+ * @argv: array of command
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
