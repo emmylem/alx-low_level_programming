ee56264202be88170253ce6fa1798a68f7303750
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-04 10:14:29 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,11 +10,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	printf("argc = %d\n", argc);
-
 	for (i = 0; i < argc; i++)
 	{
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
