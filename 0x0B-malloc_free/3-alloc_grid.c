8e20608524b962e47bec0bb9df265868a933f6ab
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-07 13:58:49 -0800

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 
 	if (width < 1 || height < 1)
 		return (NULL);
-	arr = malloc(height * sizeof(int*));
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
