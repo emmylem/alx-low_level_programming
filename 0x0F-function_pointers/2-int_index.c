a3c578eac2758811b0dfbc7f85fce7fbddd4d9bc
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-16 07:52:30 -0800

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - searches for integee
+ * @array: int pointer
+ * @size: int
+ * @cmp: function pointer
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
