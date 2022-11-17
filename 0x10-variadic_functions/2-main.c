713f6301979e01f189aaf4eb67bb77c1a0257cd8
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-17 13:32:19 -0800

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * main - checks code
+ * Return: 0
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Ben", "Dollar");
+	return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - print strings
+ * @separator: string
+ * @n: number of args
+ * Return: type
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+
+	unsigned int i;
+	char *args;
+
+	va_start(vl, n);
+
+	for (i = 0; i < n; i++)
+	{
+		args = va_arg(vl, char*);
+
+		if (args)
+		{
+			printf("%s", args);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (i < (n - 1) && separator)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	putchar('\n');
+
+	va_end(vl);
+}
