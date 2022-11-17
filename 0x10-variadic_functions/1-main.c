be8bcec0fda7cd15c2233f115ec6cce73652e842
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-17 08:07:08 -0800

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-main.c
@@ -0,0 +1,10 @@
+#include "variadic_functions.h"
+/**
+ * main - checks code
+ * Return: 0
+ */
+int main(void)
+{
+	print_numbers(" | ", 4, 0, 98, -1025, 400);
+	return (0);
+}
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints numbers
+ * @separator: string
+ * @n: number of args
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vl;
+
+	unsigned int i;
+
+	va_start(vl, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d/n", va_arg(vl, unsigned int));
+
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s\n", separator);
+		}
+	}
+	putchar('\n');
+
+	va_end(vl);
+}
