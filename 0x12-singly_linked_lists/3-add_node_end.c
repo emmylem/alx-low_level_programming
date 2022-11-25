8306f94602934bd3a72a1cd4d8ed36df3814dcca
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-24 17:17:55 -0800

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,6 +30,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	{
 		return (NULL);
 	}
+	end_node->str = strdup(str);
+	end_node->len = _strlen(str);
+	end_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = end_node;
+	}
 	else
 	{
 		list = *head;
