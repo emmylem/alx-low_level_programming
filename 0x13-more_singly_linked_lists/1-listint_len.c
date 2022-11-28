448475e5658abf1789235fab54c44aaa236abce6
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-28 14:02:09 -0800

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * listint_len - Returns number of elements
+ * @h: struct
+ * Return: size_t
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int index;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		index++;
+	}
+	return (index);
+}
