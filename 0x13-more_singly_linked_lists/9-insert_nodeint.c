0b82f0b93ba70e7e2b1b240050417c9daea1f19c
emmylemk9 <benjaminosondu55@gmail.com>
2022-11-29 13:42:47 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	node2->n = n;
 	node2->next = NULL;
 
-	if (idx)
+	if (!idx)
 	{
 		node2->next = *head;
 		*head = node2;
