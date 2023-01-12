84dec03e768093877990da938cc13bcefac5d572
emmylem <benjaminosondu55@gmail.com>
2023-01-12 12:18:27 +0000

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	for (; index != 0; index++)
 	{
-		if (!head)
+		if (head == NULL)
 			return (NULL);
 		head = head->next;
 	}
