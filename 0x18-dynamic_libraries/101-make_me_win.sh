e3b48ebec9ba961d1a959b8df3bec018bc4a731f
emmylemk9 <benjaminosondu55@gmail.com>
2023-01-20 03:44:34 -0800

    update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp https://github.com/emmylem/alx-low_level_programming
-/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget https://github.com/emmylem/alx-low_level_programming
+/raw/master
+export LD_PRELOAD=rand.so
